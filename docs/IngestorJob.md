2605ce526889a2bd627e3877ff017ef462aa641e
Adam Fletcher <adam@bit.io>
2021-08-18 14:28:27 -0700

    New SDK generated by adam

---
+++ docs/IngestorJob.md
@@ -0,0 +1,17 @@
+# IngestorJob
+
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**original_filename** | **str** |  | 
+**file_type** | **str, none_type** |  | [optional] 
+**repo** | **int, none_type** |  | [optional] 
+**table** | **int, none_type** |  | [optional] 
+**state** | **int** |  | [optional] 
+**status_message** | **str** |  | [optional] 
+**retries** | **int** |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- test/test_import_json.py
+++ test/test_import_json.py
@@ -12,11 +12,11 @@ import sys
 import unittest
 
 import bitdotio
-from bitdotio.model.query_result import QueryResult
+from bitdotio.model.import_json import ImportJson
 
 
-class TestQueryResult(unittest.TestCase):
-    """QueryResult unit test stubs"""
+class TestImportJson(unittest.TestCase):
+    """ImportJson unit test stubs"""
 
     def setUp(self):
         pass
@@ -24,10 +24,10 @@ class TestQueryResult(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testQueryResult(self):
-        """Test QueryResult"""
+    def testImportJson(self):
+        """Test ImportJson"""
         # FIXME: construct object with mandatory attributes with example values
-        # model = QueryResult()  # noqa: E501
+        # model = ImportJson()  # noqa: E501
         pass
 
 
