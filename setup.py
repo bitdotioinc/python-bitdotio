d85066c84c579d38beeddd06be42db1c85a420d1
Adam Fletcher <adam@bit.io>
2021-08-21 12:11:21 -0700

    new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "bitdotio"
-VERSION = "1.0.0b4"
+VERSION = "1.0.0b5"
 # To install the library, run the following
 #
 # python setup.py install
