e4d4e5d68b68461a1b8edfe64dd4027bfc9b94ee
Adam Fletcher <adam@bit.io>
2021-08-23 19:49:01 -0700

    update setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "bitdotio"
-VERSION = "1.0.2b"
+VERSION = "1.0.0b6"
 # To install the library, run the following
 #
 # python setup.py install
@@ -18,7 +18,18 @@ VERSION = "1.0.2b"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "psycopg2 >= 2.8.6"]
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+EXTRAS = {
+    "psycopg2": [
+        "psycopg2>=2.8.6",
+    ],
+    "psycopg2-binary": [
+        "psycopg2-binary>=2.8.6",
+    ],
+}
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name=NAME,
@@ -26,12 +37,20 @@ setup(
     description="bit.io Python SDK",
     author="bit.io",
     author_email="python@bit.io",
-    url="https://bit.io/",
+    url="https://github.com/bitdotioinc/python-bitdotio",
     keywords=["bit.io", "Database", "bit.io Python SDK"],
     install_requires=REQUIRES,
+    extras_require=EXTRAS,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
-    long_description="""\
-    bit.io Python SDK  # noqa: E501
-    """
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/bitdotioinc/python-bitdotio/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
